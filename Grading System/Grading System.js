f40bdd8db7607d4c915bccdcb3b97500f86f4f21
Sirwagya Shekhar <sirwagya7@gmail.com>
2025-10-30 10:55:43 +0530

    Grading System

---
+++ Grading System/Grading System.js
@@ -0,0 +1,12 @@
+let grade = prompt("Enter your marks:");
+if (grade >= 90) {
+    console.log("A");
+} else if (grade >= 80) {
+    console.log("B");
+} else if (grade >= 70) {
+    console.log("C");
+} else if (grade >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
